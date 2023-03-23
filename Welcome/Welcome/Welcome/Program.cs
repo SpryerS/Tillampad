c81411e4e984b3afd3ab38b5126259935e891e4b
Kevin J <94826860+SpryerS@users.noreply.github.com>
2023-03-23 18:44:53 +0100

    Add files via upload

---
+++ Welcome/Welcome/Welcome/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Vad heter du? ");
+        string name = Console.ReadLine();
+
+        Welcome(name);
+    }
+
+    static void Welcome(string name)
+    {
+        Console.WriteLine("Välkommen " + name + "!");
+        Console.ReadKey();
+    }
+}
