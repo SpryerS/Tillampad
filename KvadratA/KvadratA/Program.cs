9d03cdd94ddce2442e8dada1ddae8ed38b151242
Kevin J <94826860+SpryerS@users.noreply.github.com>
2023-03-23 18:47:01 +0100

    Add files via upload

---
+++ KvadratA/KvadratA/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KvadratA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ange sidan på kvadraten: ");
+            double sida = double.Parse(Console.ReadLine());
+
+            double area = sida * sida;
+
+            Console.WriteLine("Arean av kvadraten är: " + area);
+            Console.ReadKey();
+        }
+    }
+}
